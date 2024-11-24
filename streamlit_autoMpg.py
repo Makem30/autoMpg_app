53f82d66c0bfdc76299b763fc25ae81d320ea231
Makem30 <108522109+Makem30@users.noreply.github.com>
2024-11-24 18:38:30 +0100

    Update streamlit_autoMpg.py

--- streamlit_autoMpg.py
+++ streamlit_autoMpg.py
@@ -6,6 +6,6 @@ import pandas as pd
 import altair as alt
 
 # Charger les données
-df2 = pd.read_csv('auto-mpg.csv', delimiter=",")
+df2 = pd.read_csv('auto-mpg.csv', delimiter=";")
 chart = alt.Chart(data).mark_bar().encode(x='mpg' , y='mpg').properties(
     title='Histogramme')
