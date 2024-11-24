081e378056b97230085ee36e86f258c9d7e34299
Makem30 <108522109+Makem30@users.noreply.github.com>
2024-11-24 18:48:13 +0100

    Update streamlit_autoMpg.py

--- streamlit_autoMpg.py
+++ streamlit_autoMpg.py
@@ -6,3 +6,6 @@ import numpy as np
 # Charger les données
 df2 = pd.read_csv('auto-mpg.csv', delimiter=",")
 
+chart = alt.Chart(data).mark_bar().encode(x='mpg' , y='mpg').properties(
+    title='Sepal Histogramme')
+
