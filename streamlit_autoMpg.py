3ac1b47e581acecc6f00511ccb0fba7d64d4e48d
Makem30 <108522109+Makem30@users.noreply.github.com>
2024-11-24 18:43:01 +0100

    Update streamlit_autoMpg.py

--- streamlit_autoMpg.py
+++ streamlit_autoMpg.py
@@ -2,8 +2,6 @@ import streamlit as st
 import pandas as pd 
 import altair as alt
 import numpy as np
-import pandas as pd 
-import altair as alt
 
 # Charger les données
 data = pd.read_csv('auto-mpg.csv', delimiter=",")
